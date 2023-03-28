4fd54a48c2789221dd708fc0eddf3087fc08e746
Rahul Chauhan <rahul.chauhan@cern.ch>
2023-03-28 19:04:35 +0200

    Initialising with current code

---
+++ Readme.md
@@ -0,0 +1,12 @@
+User data registration
+
+
+1. Transfer files from T1_DE_KIT_Disk to T1_DE_KIT_Temp: gfal_transfer.py
+
+2. Register files in rucio
+
+3. Create Block and Containers
+
+4. Attach files to blocks and blocks to containers
+
+5. Create rule on containers
---
+++ src/utils/create_rule.py
@@ -0,0 +1,21 @@
+
+def create_rule(client, dids, options, dry_run=False):
+
+    rse_expression = options["dest_rse_expression"]
+    lifetime = options["lifetime"] 
+    copies = options["copies"]
+    ask_approval = options["ask_approval"]
+
+    if dry_run:
+        print(f"A rule will be created on {dids} at {rse_expression} for {lifetime|"Infinity"}")
+        return ["DryRun"]
+
+    return client.add_replication_rule(
+        dids=dids,
+        copies=1,
+        rse_expression=rse_exp,
+        grouping="ALL",
+        activity="User Subscriptions",
+        priority=1,
+        lifetime = lifetime
+    )
